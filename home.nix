89e921c96938039a5fe3e6dcaf4fa7940a8209d3
Mahdi Khosravi <mmk1776@gmail.com>
2024-01-19 11:52:29 +0300

    nixos config start

---
+++ home.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "mmk";
+  home.homeDirectory = "/home/mmk";
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Mahdi Khosravi";
+    userEmail = "mmk1776@gmail.com";
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # archives
+    zip
+    unzip
+
+    # utils
+    # ripgrep # recursively searches directories for a regex pattern
+    # fzf # A command-line fuzzy finder
+
+    # misc
+    which
+    tree
+  ];
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your cusotm bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      k = "kubectl";
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
